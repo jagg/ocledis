586563f9ef76c0019032b50c386be672063714b3
Jose A. Garcia <argantonio@gmail.com>
2025-04-18 17:32:51 +0200

    Fix bench

--- bench/bench.ml
+++ bench/bench.ml
@@ -9,30 +9,29 @@ let value_pairs =
 module type Storage = sig
   type t
 
-  val create :  unit -> t
-  val put : t -> key:Kvlib.Model.Key.t -> value:Kvlib.Model.value -> unit Or_error.t
-  val get : t -> key:Kvlib.Model.Key.t -> Kvlib.Model.value option
+  val make:  Kvlib.Store.config -> t
+  val update : t -> Kvlib.Model.update_op -> unit Or_error.t
+  val get : t -> Kvlib.Model.Key.t -> Kvlib.Model.value option
 end
 
 module Bench (S : Storage) = struct
   let run_bench () =
-    let table = S.create () in
+    let table = S.make Kvlib.Store.default_config in
     List.iter value_pairs
       ~f:(fun (key, value) ->
           let key = Kvlib.Model.Key.String key in
           let value = Kvlib.Model.Num value in
-          Or_error.ok_exn @@ S.put table ~key ~value;
-          let _ = S.get table ~key in
+          let op = Kvlib.Model.Set (key, value) in
+          Or_error.ok_exn (S.update table op);
+          let _ = S.get table key in
           ()
         )
 end 
 
-module Bench_hashtbl = Bench(Kvlib.Storage_hashtbl)
-module Bench_wal = Bench(Kvlib.Write_ahead_log)
+module Bench_wal = Bench(Kvlib.Store)
 
 let benchmarks =
-  [ "Hashtable", Bench_hashtbl.run_bench
-  ; "WAL", Bench_wal.run_bench]
+  [ "WAL", Bench_wal.run_bench]
 
 let () =
   List.map benchmarks ~f:(fun (name, test) ->
